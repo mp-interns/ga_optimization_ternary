c7c9777a592d8ad37a70b2bee213c28deec7412f
Anubhav Jain <ajain@lbl.gov>
2012-07-13 12:12:51 -0700

    improve plotting, use correct reference

--- ga_optimization_ternary/plotting.py
+++ ga_optimization_ternary/plotting.py
@@ -5,6 +5,7 @@ from __future__ import division
 Created on Jul 6, 2012
 '''
 from ga_optimization_ternary.database import M_Database, Stats_Database, MAX_CAND, MAX_GOOD
+from ga_optimization_ternary.utils import get_reference_array
 
 __author__ = "Anubhav Jain"
 __copyright__ = "Copyright 2012, The Materials Project"
@@ -54,7 +55,7 @@ class PerformancePlot():
         plt.annotate(label, xy = (x[(int)(MAX_GOOD*3/4)], y[(int)(MAX_GOOD*3/4)]), xytext = (-5, 5), color=color, textcoords = 'offset points', ha = 'right', va = 'bottom', fontname=self.fontname, fontsize=self.fontsize, arrowprops = None)
     
     def get_reference_data(self):
-        x = [0, MAX_CAND]
+        x = [0, get_reference_array()[15]]
         y = [0, MAX_GOOD]
         plt.errorbar(x, y, lw=self.lw, color="black")
         plt.annotate("random", xy = (x[1] * 0.66, y[1] * 0.66), xytext = (-15, -30), color="black", textcoords = 'offset points', ha = 'left', va = 'bottom', fontname=self.fontname, fontsize=self.fontsize, arrowprops = None)
@@ -78,8 +79,7 @@ class ComparisonPlot():
         plt.setp(plt.gca().get_yticklabels(), fontname=self.fontname, fontsize=self.fontsize)
         plt.xlim((0, self.num_exps))
         self.get_reference()
-        self.get_data()
-        
+        self.get_data() 
     
     def get_data(self):
         x = []
@@ -88,7 +88,7 @@ class ComparisonPlot():
         for data in self.stats_process.find({}, sort = [("ten", pymongo.ASCENDING)]):
             x.append(idx)
             idx = idx + 1
-            y.append((10/MAX_GOOD * MAX_CAND)/data["ten"])
+            y.append(get_reference_array()[10]/data["ten"])
         plt.errorbar(x, y, lw=self.lw, color="red")
         plt.annotate("10 materials", xy = (x[50], y[50]), xytext = (10, 0), color="red", textcoords = 'offset points', ha = 'left', va = 'bottom', fontname=self.fontname, fontsize=self.fontsize, arrowprops = None)
         
@@ -98,7 +98,7 @@ class ComparisonPlot():
         for data in self.stats_process.find({}, sort = [("all", pymongo.ASCENDING)]):
             x.append(idx)
             idx = idx + 1
-            y.append(MAX_CAND/data["all"])
+            y.append(get_reference_array()[15]/data["all"])
         plt.errorbar(x, y, lw=self.lw, color="blue")
         plt.annotate("all 15 materials", xy = (x[100], y[100]), xytext = (-25, 10), color="blue", textcoords = 'offset points', ha = 'right', va = 'top', fontname=self.fontname, fontsize=self.fontsize, arrowprops = None)
     
@@ -107,36 +107,59 @@ class ComparisonPlot():
         y = [1, 1]
         plt.plot(x, y, "k--", lw=3)
 
+
 class ParametersPlot():
 
     def __init__(self):
-        plt.figure(2)
+        plt.figure(1, figsize=(16,8))
         self.db = Stats_Database()
         self.stats_process = self.db._stats_process
         self.num_exps = self.stats_process.count()
         
-        self.get_data("crossover_fnc")
+        params = ["crossover_fnc", "popsize", "selection_fnc", "elitism_num", "fitness_fnc", "initialization_fnc"]
+        
+        plt.subplot(2, 3, 1)
+        for idx, param in enumerate(params):
+            plt.subplot(2, 3, idx+1)
+            self.get_data(param)
         
     
-    def get_data(self, param):
-        plt.title(self.get_pretty_name(param))
-        parameter = "crossover_fnc"
+    def get_data(self, parameter):
+        plt.title(self.get_pretty_name(parameter))
         
-        labels = ["Baseline", "System"]
-        data = [3.75, 4.75]
+        data = []
+        labels = self.stats_process.distinct("parameters."+parameter)
+        labels.sort()
+        for label in labels:
+            # get the best
+            best = self.stats_process.find({"parameters."+parameter:label}, sort=[("ten",pymongo.ASCENDING)])[0]
+            data.append(get_reference_array()[10]/best["ten"])
         
-        
+        pretty_labels = [self.get_pretty_name(n) for n in labels]
         xlocations = np.array(range(len(data))) + 0.5
         width = 0.75
         plt.bar(xlocations, data, width=width)
-        plt.xticks(xlocations + width/2, labels)
+        plt.xticks(xlocations + width/2, pretty_labels)
         plt.xlim(0, xlocations[-1] + width * 2)
     
     def get_pretty_name(self, ugly_name):
         d = {}
         d['crossover_fnc'] = "Crossover Function"
-        
-        return d[ugly_name]
+        d['selection_fnc'] = "Selection Function"
+        d['fitness_fnc'] = "Fitness Function"
+        d['G1DListCrossoverSinglePoint'] = "Single Point"
+        d['G1DListCrossoverTwoPoint'] = "Two Point"
+        d['G1DListCrossoverUniform'] = "Uniform"
+        d['GRouletteWheel'] = "Roulette"
+        d['GTournamentSelector'] = "Tournament"
+        d['GUniformSelector'] = "Uniform"
+        d['eval_fitness_partial'] = "Partial"
+        d['eval_fitness_simple'] = "All-or-Nothing"
+        d['initialization_fnc'] = "Initialization Function"
+        d['G1DListInitializatorInteger'] = "Random"
+        d['popsize'] = "Population size"
+        d['elitism_num'] = "Number of Elite"
+        return d.get(ugly_name, ugly_name)
     
 class TenAllPlot():
 
